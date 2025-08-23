9b1cadfe771e36c6c778d5e931cd077ce7d31b0b
adamgofast <adam@gofastcrushgoals.com>
2025-08-23 18:15:09 -0400

    admin login

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home.jsx';
+import AdminHome from './pages/AdminHome.jsx';
 import UserAdmin from './pages/UserAdmin.jsx';
 import UserDashboard from './pages/UserDashboard.jsx';
 
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <div className="min-h-screen bg-background">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<AdminHome />} />
           <Route path="/user-admin" element={<UserAdmin />} />
           <Route path="/user-dashboard" element={<UserDashboard />} />
         </Routes>
